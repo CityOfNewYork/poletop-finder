d0df98d6ceabc7a09a0562aa9c9675899534b9cf
timkeane <t.d.keane@gmail.com>
2020-08-04 15:30:30 -0400

    tweak cluster params

--- src/js/poletop.js
+++ src/js/poletop.js
@@ -8,8 +8,8 @@
 * @const {Object<string, string>}
 */
 const poletop = {
-  CLUSTER_DISTANCE: 75,
-  CLUSTER_RADIUS_DENOMINATOR: 10,
+  CLUSTER_DISTANCE: 100,
+  CLUSTER_RADIUS_DENOMINATOR: 6,
   GEOCLIENT_URL: 'https://maps.nyc.gov/geoclient/v2/search.json?app_key=74DF5DB1D7320A9A2&app_id=nyc-lib-example',
   DIRECTIONS_URL: 'https://maps.googleapis.com/maps/api/js?&sensor=false&libraries=visualization',
   PUBLIC_DATA_URL: 'https://data.cityofnewyork.us/resource/tbgj-tdd6.csv?$select=x_coord%20as%20x,y_coord%20as%20y,franchisee,on_street,cross_street_1,cross_street_2,borough,pole_type,bid_advisory,historic_advisory,park_advisory,school_advisory,zipcode,community_board,council_district,scenic_landmark_advisory&$where=equipment_installed_yes_no=%27Y%27',
